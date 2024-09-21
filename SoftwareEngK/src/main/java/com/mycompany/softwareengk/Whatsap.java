a15288e0842054c309bf7704e82d16215dc3b17e
mrbassx <abassiddrisu9222@gmail.com>
2024-09-21 11:06:16 +0000

    commit

---
+++ SoftwareEngK/src/main/java/com/mycompany/softwareengk/Whatsap.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.softwareengk;
+
+/**
+ *
+ * @author the-brain
+ */
+public class Whatsap {
+    
+    
+    public static void main(String []aaa){
+    
+    product object = new product("apple",888888888);
+//    object.sayhello();700
+    System.out.println(object.getImage());
+    
+    }
+    
+}
--- SoftwareEngK/src/main/java/com/mycompany/softwareengk/product.java
+++ SoftwareEngK/src/main/java/com/mycompany/softwareengk/product.java
@@ -10,18 +10,61 @@ package com.mycompany.softwareengk;
  */
 class product {
     
-  private  String name ;
-  private int image;
-  private double price ;
+   private String name ;
+   private int image;
+   private double price ;
+
+    public product(String name, int image, double price) {
+        this.name = name;
+        this.image = image;
+        this.price = price;
+    }
+
+    public product(int image) {
+        this.image = image;
+    }
+
+    public product() {
+    }
+
+    public product(double price) {
+        this.price = price;
+    }
+ 
+
+    public product(String name, int image) {
+        this.name = name;
+        this.image = image;
+    }
+    
+    
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
   
   
-  product (String Name,int Image,double Price){
-  
-     this.name = Name ;
-     this.image = Image;
-     this.price = Price;
-  
-  }
+ 
   
     
 }
