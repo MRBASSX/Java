b24970f23fbb20f8fed2a74d07b73048f832aae0
mrbassx <abassiddrisu9222@gmail.com>
2024-09-29 02:27:13 +0000

    commit

---
+++ AnimalKingdom/src/main/java/com/mycompany/animalkingdom/AnimalKingdom.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.animalkingdom;
+
+/**
+ *
+ * @author the-brain
+ */
+public class AnimalKingdom {
+
+    public static void main(String[] args) {
+       
+        Animal  AnimalObject = new Animal();
+        AnimalObject.Sound();
+        
+        Cat CatObject = new Cat();
+        CatObject.Sound();
+       
+         
+         Sheep SheepObject = new Sheep();
+         SheepObject.Sound();
+         
+         
+         Dog DogObject = new Dog();
+         DogObject.Sound();
+         
+         
+    }
+}
